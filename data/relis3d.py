e25ea3583112faea32d97f7a2f01ffe41c1baa2f
mahmed10 <mahmed10@umbc.edu>
2021-07-29 15:34:52 -0400

    relis dataloader

---
+++ data/relis3d.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import torch
+
+class Relis3D(torch.utils.data.Dataset):
+	def __init__(self, train, path_list):
+		self.train = train
+		
+		with open(path_list, "r") as file:
+			self.imgs = file.readlines()
+
+		self.masks = [
+			path.replace("pylon_camera_node", "pylon_camera_node_label_id").replace(".jpg", ".png")
+			for path in self.imgs
+		]
+
+	def __getitem__(self, index):
+		img_path, mask_path = self.imgs[index], self.masks[index]
+		img, mask = Image.open(img_path).convert('RGB'), Image.open(mask_path)
+
+		return img, mask
+
+	def __len__(self):
+		return len(self.imgs)
