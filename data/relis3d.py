a319e8d3c1952d5b0a572a18ca7c75f3f03bdb40
mahmed10 <mahmed10@umbc.edu>
2021-07-29 16:21:32 -0400

    data loader

--- data/relis3d.py
+++ data/relis3d.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
 import torch
+from PIL import Image
 
 class Relis3D(torch.utils.data.Dataset):
 	def __init__(self, train, path_list):
 		self.train = train
-		
+
 		with open(path_list, "r") as file:
 			self.imgs = file.readlines()
 
@@ -15,7 +16,7 @@ class Relis3D(torch.utils.data.Dataset):
 		]
 
 	def __getitem__(self, index):
-		img_path, mask_path = self.imgs[index], self.masks[index]
+		img_path, mask_path = self.imgs[index].rstrip(), self.masks[index].rstrip()
 		img, mask = Image.open(img_path).convert('RGB'), Image.open(mask_path)
 
 		return img, mask
