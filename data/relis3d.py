78145e5dd200945243fb5b0ad3ca78030e6b8349
mahmed10 <mahmed10@umbc.edu>
2021-10-26 01:37:33 -0400

    train function added

--- data/relis3d.py
+++ data/relis3d.py
@@ -5,13 +5,34 @@ from PIL import Image
 import yaml
 import data.edge_utils as edge_utils
 
-num_classes = 19
+num_classes = 2
+ignore_label = 0
+
+def colorize_mask(mask):
+	# mask: numpy array of the mask
+	new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
+	new_mask.putpalette(palette)
+	return new_mask
 
 
 class Relis3D(torch.utils.data.Dataset):
-	def __init__(self, train, path_list, data_mode):
-		self.train = train
+	def __init__(self, path_list, train = 'train', data_mode ='rgb', joint_transform=None, sliding_crop=None,
+		transform=None, target_transform=None, dump_images=False, cv_split=None, eval_mode=False, 
+		eval_scales=None, eval_flip=False):
+
+		self.mode = train
 		self.data_mode = data_mode
+		self.joint_transform = joint_transform
+		self.sliding_crop = sliding_crop
+		self.transform = transform
+		self.target_transform = target_transform
+		self.dump_images = dump_images
+		self.eval_mode = eval_mode
+		self.eval_flip = eval_flip
+		self.eval_scales = None
+
+		if eval_scales != None:
+			self.eval_scales = [float(scale) for scale in eval_scales.split(",")]
 
 		with open(path_list, "r") as file:
 			self.imgs = file.readlines()
@@ -32,30 +53,78 @@ class Relis3D(torch.utils.data.Dataset):
 			relis3dyaml = yaml.safe_load(stream)
 		self.learning_map = relis3dyaml['learning_map']
 
+		self.mean_std = ([0.54218053, 0.64250553, 0.56620195], [0.54218052, 0.64250552, 0.56620194])
+
 	def convert_label(self, label, inverse=False):
 		temp = label.copy()
 		for k, v in self.learning_map.items():
 			label[temp == k] = v
 		return label
 
+	def _eval_get_item(self, img, mask, scales, flip_bool):
+		return_imgs = []
+		for flip in range(int(flip_bool)+1):
+			imgs = []
+			if flip :
+				img = img.transpose(Image.FLIP_LEFT_RIGHT)
+			for scale in scales:
+				w,h = img.size
+				target_w, target_h = int(w * scale), int(h * scale) 
+				resize_img =img.resize((target_w, target_h))
+				tensor_img = transforms.ToTensor()(resize_img)
+				final_tensor = transforms.Normalize(*self.mean_std)(tensor_img)
+				imgs.append(tensor_img)
+			return_imgs.append(imgs)
+		return return_imgs, mask
+
 
 	def __getitem__(self, index):
 		img_path, mask_path = self.imgs[index].rstrip(), self.masks[index].rstrip() 
 		#print("img_path path: ", img_path)
 		#print("mask_path path: ", mask_path)
-		img, mask = Image.open(img_path).convert('RGB'), Image.open(mask_path)
+		img = Image.open(img_path).convert('RGB')
+		img = img.resize((int(img.size[0]*0.75),int(img.size[1]*0.75)))
+		mask = Image.open(mask_path)
+		mask = np.array(mask.resize((int(mask.size[0]*0.75),int(mask.size[1]*0.75))))
 
-		mask = np.array(mask)
+
+		mask = mask[:, :]
+		print(mask)
+
 		mask_copy = self.convert_label(mask)
-		mask = Image.fromarray(mask_copy.astype(np.uint8))
 
-		_edgemap = mask_copy
-		del mask_copy
+		mask = Image.fromarray(mask_copy.astype(np.uint8))
+		# Image Transformations
+
+
+		# if self.joint_transform is not None:
+		# 	img, mask = self.joint_transform(img, mask)
+		if self.transform is not None:
+			img = self.transform(img)
+		if self.target_transform is not None:
+			mask = self.target_transform(mask)
+		if self.mode == 'test':
+			return img, mask, img_name, item['img']
+
+		_edgemap = mask.numpy()
 		_edgemap = edge_utils.mask_to_onehot(_edgemap, num_classes)
 
 		_edgemap = edge_utils.onehot_to_binary_edges(_edgemap, 2, num_classes)
 
 		edgemap = torch.from_numpy(_edgemap).float()
+		# img = torch.from_numpy(img).float()
+		# mask = torch.from_numpy(mask).float()
+
+		# Debug
+		if self.dump_images:
+			outdir = '../../dump_imgs_{}'.format(self.mode)
+			os.makedirs(outdir, exist_ok=True)
+			out_img_fn = os.path.join(outdir, img_name + '.png')
+			out_msk_fn = os.path.join(outdir, img_name + '_mask.png')
+			mask_img = colorize_mask(np.array(mask))
+			img.save(out_img_fn)
+			mask_img.save(out_msk_fn)
+		print(mask)
 		return img, mask, edgemap
 
 	def __len__(self):
---
+++ train.py
@@ -0,0 +1,89 @@
+import os
+import argparse
+import numpy as np
+from PIL import Image
+
+import torch 
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+from torchvision import transforms, datasets, utils
+from tqdm import tqdm
+
+from utils import get_cityscapes_data
+
+from model import UNET
+
+
+parser = argparse.ArgumentParser(description='Benchmarking')
+parser.add_argument('--dataset', type=str, default='relis3d')
+parser.add_argument('--train_mode', type=str, default='train')
+parser.add_argument('--train_path_list', type=str, default='trainlist.txt')
+parser.add_argument('--val_path_list', type=str, default='vallist.txt')
+parser.add_argument('--data_mode', type=str, default='rgb')
+parser.add_argument('--model_path', type=str, default='YOUR-MODEL-PATH')
+parser.add_argument('--load_model', type=bool, default=True)
+
+parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--epoches', type=int, default=5)
+parser.add_argument('--learning_rate', type=float, default=0.0005)
+
+args = parser.parse_args()
+
+epoches = args.epoches
+
+if torch.cuda.is_available():
+	args.device = 'cuda:0'
+else:
+	args.device = "cpu"
+
+def train(data, model, optimizer, loss_fn, device):
+	print('Entering into train function')
+	loss_values = []
+	data = tqdm(data)
+	for index, batch in enumerate(data): 
+		X, y = batch
+		X, y = X.to(device), y.to(device)
+		preds = model(X)
+		loss = loss_fn(preds, y)
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+	return loss.item()
+
+def main():
+	global epoch
+	epoch = 0 
+	train_set = get_cityscapes_data(args.train_path_list, args.batch_size)
+	print('Data Loaded Successfully!')
+	loss_vals = []
+
+	# Defining the model, optimizer and loss function
+	unet = UNET(in_channels=3, classes=20).to(args.device).train()
+	optimizer = optim.Adam(unet.parameters(), lr=args.learning_rate)
+	loss_function = nn.CrossEntropyLoss(ignore_index=255)
+
+	if args.load_model == True:
+		checkpoint = torch.load(args.model_path)
+		unet.load_state_dict(checkpoint['model_state_dict'])
+		optimizer.load_state_dict(checkpoint['optim_state_dict'])
+		epoch = checkpoint['epoch']+1
+		loss_vals = checkpoint['loss_values']
+		print("Model successfully loaded!") 
+
+	for e in range(epoch, epoches):
+		print(f'Epoch: {e}')
+		loss_val = train(train_set, unet, optimizer, loss_function, args.device)
+		loss_vals.append(loss_val) 
+		torch.save({
+			'model_state_dict': unet.state_dict(),
+			'optim_state_dict': optimizer.state_dict(),
+			'epoch': e,
+			'loss_values': loss_vals
+		}, args.model_path)
+		print("Epoch completed and model successfully saved!")
+
+if __name__ == '__main__':
+	main()
