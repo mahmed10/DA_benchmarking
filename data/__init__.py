834451e42c2cf933621960c1c55253c53a58b00c
mahmed10 <mahmed10@umbc.edu>
2021-11-22 14:20:28 -0500

    dataloader update with semantickitti

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,12 @@
-from data import relis3d
+from data import relis3d,semantickitti
 from torch.utils.data import DataLoader
 
-def setup_loaders(path_list, batch_size):
-	data = relis3d.Relis3D(path_list)
-	data_loaded = DataLoader(data, batch_size=batch_size)
-	return data_loaded
+def setup_loaders(dataset, path_list, batch_size):
+	if(dataset == 'rellis3d'):
+		data = relis3d.Relis3D(path_list)
+		data_loaded = DataLoader(data, batch_size=batch_size)
+		return data_loaded
+	elif(dataset == 'semantickitti'):
+		data = semantickitti.SemanticKitti(path_list)
+		data_loaded = DataLoader(data, batch_size=batch_size)
+		return data_loaded
--- data/semantickitti.py
+++ data/semantickitti.py
@@ -6,13 +6,8 @@ import yaml
 
 from torchvision import transforms, datasets
 
-def colorize_mask(mask):
-	new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
-	new_mask.putpalette(palette)
-	return new_mask
 
-
-class Relis3D(torch.utils.data.Dataset):
+class SemanticKitti(torch.utils.data.Dataset):
 	def __init__(self, path_list, train = 'train', data_mode ='rgb', scale = 1, target_width = 572, target_height = 572):
 
 		self.mode = train
@@ -25,14 +20,13 @@ class Relis3D(torch.utils.data.Dataset):
 			self.imgs = file.readlines()
 
 		self.masks = [
-			path.replace("/pylon_camera_node/", "/pylon_camera_node_label_id/")
-			.replace("/Rellis_3D_pylon_camera_node/", "/Rellis_3D_pylon_camera_node_label_id/")
-			.replace(".jpg", ".png")
+			path.replace("/data_tracking_image_2/", "/kitti-step/panoptic_maps/")
+			.replace("/training/image_02/", "/train/")
 			for path in self.imgs
 		]
 
 
-		with open("./dataset/Rellis3d/Rellis_3D.yaml", 'r') as stream:
+		with open("./dataset/SemanticKitti/semanticKitti.yaml", 'r') as stream:
 			relis3dyaml = yaml.safe_load(stream)
 		self.learning_map = relis3dyaml['learning_map']
 
@@ -42,24 +36,6 @@ class Relis3D(torch.utils.data.Dataset):
 			label[temp== k] = v
 		return label
 
-	# Till now did not use this but will use this in future
-	def _eval_get_item(self, img, mask, scales, flip_bool):
-		return_imgs = []
-		for flip in range(int(flip_bool)+1):
-			imgs = []
-			if flip :
-				img = img.transpose(Image.FLIP_LEFT_RIGHT)
-			for scale in scales:
-				w,h = img.size
-				target_w, target_h = int(w * scale), int(h * scale) 
-				resize_img =img.resize((target_w, target_h))
-				tensor_img = transforms.ToTensor()(resize_img)
-				final_tensor = transforms.Normalize(*self.mean_std)(tensor_img)
-				imgs.append(tensor_img)
-			return_imgs.append(imgs)
-		return return_imgs, mask
-
-
 	def __getitem__(self, index):
 		img_path, mask_path = self.imgs[index].rstrip(), self.masks[index].rstrip()
 
@@ -67,7 +43,7 @@ class Relis3D(torch.utils.data.Dataset):
 		mask = Image.open(mask_path)
 
 		w,h = img.size
-		target_w, target_h = int(w * self.scale), int(h * self.scale) 
+		target_w, target_h = int(w * self.scale), int(h * self.scale)
 		target_w, target_h = self.target_w, self.target_h
 		img = img.resize((target_w, target_h))
 		img = transforms.ToTensor()(img)
