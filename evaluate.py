1a0a7f724a42fc971772ac2413b99e4e62c307cb
mahmed10 <mahmed10@umbc.edu>
2021-12-16 23:59:00 -0500

    train and evaluate update

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,11 @@
 import torch
 import numpy as np
+import numpy
+from tqdm import tqdm
+from domain_adaptation.utils.func import per_class_iu, fast_hist
+import matplotlib.patches as mpatches
+from torchvision import transforms
+import matplotlib.pyplot as plt
 SMOOTH = 1e-6
 def iou_calculation(batch, model, device):
 	X, labels = batch
@@ -8,22 +14,66 @@ def iou_calculation(batch, model, device):
 	output = pred_main.cpu().data[0].numpy()
 	output = output.transpose(1, 2, 0)
 	pred = np.argmax(output, axis=2)
-
 	outputs = torch.tensor(pred.reshape((1, pred.shape[0], pred.shape[1]))).cpu()
 	labels = torch.tensor(labels).cpu()
-	# print(outputs.size())
-	# print(labels.size())
 	intersection = (outputs & labels).float().sum((1, 2)) 
 	union = (outputs | labels).float().sum((1, 2)) 
 	iou = (intersection + SMOOTH) / (union + SMOOTH)
-	# print(iou)
-	# outputs = model(X)[1]
-	# outputs = torch.nn.functional.softmax(outputs, dim=1)
-	# outputs = torch.argmax(outputs, dim=1)
-	# outputs = torch.tensor(outputs.reshape((1, outputs.shape[0], outputs.shape[1])))
-	# labels = torch.tensor(labels)
-	# intersection = (outputs & labels).float().sum((1, 2)) 
-	# union = (outputs | labels).float().sum((1, 2)) 
-	# iou = iou + (intersection + SMOOTH) / (union + SMOOTH)
-	#iou = torch.clamp(20 * (iou - 0.5), 0, 10) / 10
 	return iou
+
+def per_class_iou_calculataion(net, target_loader, device):
+	targetloader_iter = enumerate(target_loader)
+	hist = np.zeros((9,9))
+	for i_iter in tqdm(range(1000)):
+		_, batch = targetloader_iter.__next__()
+		image, label = batch
+		image, label = image.to(device), label
+		with torch.no_grad():
+			pred_main = net(image)[1]
+			output = pred_main.cpu().data[0].numpy()
+			output = output.transpose(1, 2, 0)
+			output = np.argmax(output, axis=2)
+		label = label.numpy()[0]
+		hist += fast_hist(label.flatten(), output.flatten(), 9)
+	inters_over_union_classes = per_class_iu(hist)
+	return inters_over_union_classes
+
+def figure_draw(batch, net, device):
+	img, mask= batch
+	X, y = img.to(device), mask.to(device)
+	pred_main = net(X)[1]
+	output = pred_main.cpu().data[0].numpy()
+	output = output.transpose(1, 2, 0)
+	pred = np.argmax(output, axis=2)
+
+	plt.figure()
+	plt.title('Image')
+	trans = transforms.ToPILImage()
+	plt.imshow(trans(img[0]))
+
+	plt.figure()
+	plt.title('Mask GT')
+	values = np.unique(mask[0].ravel())
+	im = plt.imshow(mask[0], cmap= 'tab20')
+	# get the colors of the values, according to the 
+	# colormap used by imshow
+	colors = [ im.cmap(im.norm(value)) for value in values]
+	# create a patch (proxy artist) for every color 
+	patches = [ mpatches.Patch(color=colors[i], label="Level {l}".format(l=values[i]) ) for i in range(len(values)) ]
+	# put those patched as legend-handles into the legend
+	plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+
+
+	plt.figure()
+	plt.title('Mask pred')
+	values = np.unique(pred.ravel())
+	im = plt.imshow(pred, cmap= 'tab20')
+	# get the colors of the values, according to the 
+	# colormap used by imshow
+	colors = [ im.cmap(im.norm(value)) for value in values]
+	# create a patch (proxy artist) for every color 
+	patches = [ mpatches.Patch(color=colors[i], label="Level {l}".format(l=values[i]) ) for i in range(len(values)) ]
+	# put those patched as legend-handles into the legend
+	plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+
+	plt.show()
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import os
 import argparse
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy
+import numpy as np
 
 import torch 
 import torch.optim as optim
@@ -16,10 +20,15 @@ from models.deeplabv2 import get_deeplab_v2
 
 from evaluate import iou_calculation
 from domain_adaptation.train_UDA import train_domain_adaptation
+from domain_adaptation.train_Sonly import train_source_only
 
+from torchvision import transforms
 
 parser = argparse.ArgumentParser(description='Benchmarking')
-parser.add_argument('--da_mode', type=str, default='SingleDA_SingleBranch_')
+# parser.add_argument('--da_mode', type=str, default='SingleDA_')
+parser.add_argument('--da_mode', type=str, default='Sourceonly_')
+parser.add_argument('--branch_mode', type=str, default='SingleBranch_')
+parser.add_argument('--class_mode', type=str, default='CommonClasses_')
 parser.add_argument('--source_dataset', type=str, default='rellis3d')
 parser.add_argument('--target_dataset', type=str, default='semantickitti')
 parser.add_argument('--train_mode', type=str, default='train')
@@ -29,7 +38,8 @@ parser.add_argument('--data_mode', type=str, default='rgb')
 parser.add_argument('--in_channels', type=int, default=3)
 parser.add_argument('--image_width', type=int, default=572)
 parser.add_argument('--image_height', type=int, default=572)
-parser.add_argument('--num_classes', type=int, default=10)
+parser.add_argument('--num_classes', type=int, default=9)
+parser.add_argument('--train_data_size', type=int, default=1000)
 parser.add_argument('--model_path', type=str, default='./checkpoints/')
 parser.add_argument('--model_name', type=str, default='')
 parser.add_argument('--load_model', type=bool, default=False)
@@ -37,11 +47,12 @@ parser.add_argument('--multi_level', type=bool, default=False)
 
 parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--epoches', type=int, default=100)
+parser.add_argument('--start_epoches', type=int, default=0)
 parser.add_argument('--learning_rate', type=float, default=0.0005)
 
 args = parser.parse_args()
 
-model_path = args.model_path+args.da_mode+'_Source_'+args.source_dataset+'_Target_'+args.target_dataset
+model_path = args.model_path+args.da_mode+args.branch_mode+args.class_mode+'_Source_'+args.source_dataset+'_Target_'+args.target_dataset
 
 try:
 	os.mkdir(model_path)
@@ -49,7 +60,7 @@ except OSError:
 	print(os.path.abspath(model_path) + ' folder already existed')
 else:
 	print(os.path.abspath(model_path) + ' folder created')
-args.model_path = args.model_path+args.da_mode+'_Source_'+args.source_dataset+'_Target_'+args.target_dataset+'/'
+args.model_path = model_path+'/'
 del model_path
 
 epoches = args.epoches
@@ -60,70 +71,38 @@ if torch.cuda.is_available():
 else:
 	args.device = "cpu"
 
-def train(data, model, optimizer, loss_fn, device):
-	print('Entering into train function')
-	loss_values = []
-	data = tqdm(data)
-	for index, batch in enumerate(data): 
-		X, y = batch
-		# print(X.size())
-		# print(y.size())
-		# print(X)
-		# print(y)
-		X, y = X.to(device), y.to(device)
-		preds = model(X)
-		loss = loss_fn(preds, y)
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-	return loss.item()
+def check_dataloader(loader, source):
+	targetloader_iter = enumerate(loader)
+	for i in range(7):
+		_, batch = targetloader_iter.__next__()
+		img, mask= batch
+		trans = transforms.ToPILImage()
+		plt.figure()
+		plt.title('Image')
+		plt.imshow(trans(img[0]))
+		plt.savefig(args.model_path+source+'_image'+repr(i)+'.png')
+		plt.figure()
+		plt.title('Mask GT')
+		values = np.unique(mask[0].ravel())
+		im = plt.imshow(mask[0])
+		colors = [ im.cmap(im.norm(value)) for value in values]
+		patches = [ mpatches.Patch(color=colors[i], label="Level {l}".format(l=values[i]) ) for i in range(len(values)) ]
+		plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+		plt.savefig(args.model_path+source+'_mask'+repr(i)+'.png')
+
 
 def main():
-	global epoch
-	epoch = 0 
 	args.source_loader = data.setup_loaders(args.source_dataset, args.train_path_list, args.batch_size)
+	check_dataloader(args.source_loader, 'source')
 	args.target_loader = data.setup_loaders(args.target_dataset, args.val_path_list, args.batch_size)
+	check_dataloader(args.target_loader, 'target')
 	print('Data Loaded Successfully!')
 
-	# # Defining the model, optimizer and loss function
-	# unet = UNET(in_channels=3, classes=10).to(args.device).train()
-	# optimizer = optim.Adam(unet.parameters(), lr=args.learning_rate)
-	# loss_function = nn.CrossEntropyLoss(ignore_index=255)
-
-	# if args.load_model == True:
-	# 	checkpoint = torch.load(model_path)
-	# 	unet.load_state_dict(checkpoint['model_state_dict'])
-	# 	optimizer.load_state_dict(checkpoint['optim_state_dict'])
-	# 	epoch = checkpoint['epoch']+1
-	# 	loss_vals = checkpoint['loss_values']
-	# 	train_ious = checkpoint['train_iou']
-	# 	print("Model successfully loaded!") 
-
-	# for e in range(epoch, epoches):
-	# 	print(f'Epoch: {e}')
-	# 	loss_val = train(train_set, unet, optimizer, loss_function, args.device)
-	# 	loss_vals.append(loss_val)
-	# 	iou =  iou_calculation(train_set, unet,args.device)
-	# 	train_ious.append(iou)
-	# 	print('train iou', iou)
-	# 	# iou =  iou_calculation(val_set, unet,args.device)
-	# 	# val_ious.append(iou)
-	# 	# print('val iou', iou)
-	# 	torch.save({
-	# 		'model_state_dict': unet.state_dict(),
-	# 		'optim_state_dict': optimizer.state_dict(),
-	# 		'epoch': e,
-	# 		'loss_values': loss_vals,
-	# 		'train_iou': train_ious
-	# 	}, model_path + 'epoch_'+str(e).zfill(4))
-	# 	# torch.save({
-	# 	# 	'val_iou' : val_ious
-	# 	# }, model_path + 'val_iou_'+args.dataset)
-	# 	print("Epoch completed and model successfully saved!")
-
-	#model = get_deeplab_v2(num_classes=10, multi_level=True)
 	args.model = UNET(in_channels=args.in_channels, classes=args.num_classes).to(args.device)
-	train_domain_adaptation(args)
+	if(args.da_mode == 'SingleDA_'):
+		train_domain_adaptation(args)
+	if(args.da_mode == 'Sourceonly_'):
+		train_source_only(args)
 
 if __name__ == '__main__':
 	main()
