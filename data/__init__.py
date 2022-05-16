b890af9d8037cbd2fe1758f81a7373f5646d7c62
Masud Ahmed <60801849+mahmed10@users.noreply.github.com>
2022-05-16 10:26:33 -0400

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,16 +1,48 @@
-from data import relis3d, semantickitti, cityscapes
-from torch.utils.data import DataLoader
+from data import relis3d, semantickitti, cityscapes, ourdataset
+from torch.utils.data import DataLoader, ConcatDataset
 
 def setup_loaders(dataset, path_list, batch_size):
-	if(dataset == 'rellis3d'):
-		data = relis3d.Relis3D(path_list)
-		data_loaded = DataLoader(data, batch_size=batch_size)
-		return data_loaded
-	elif(dataset == 'semantickitti'):
-		data = semantickitti.SemanticKitti(path_list)
-		data_loaded = DataLoader(data, batch_size=batch_size)
-		return data_loaded
-	elif(dataset == 'cityscapes'):
-		data = cityscapes.CityScapes(path_list)
-		data_loaded = DataLoader(data, batch_size=batch_size)
-		return data_loaded
+	combined_data = []
+	for i in range(len(dataset)):
+		if(dataset[i] == 'rellis3d'):
+			data = relis3d.Relis3D(path_list[i])
+			data_loader = DataLoader(data, batch_size=batch_size)
+			if (len(dataset) == 1):
+				return data_loader
+			else:
+				combined_data.append(data)
+				del data_loader
+				del data
+		if(dataset[i] == 'semantickitti'):
+			data = semantickitti.SemanticKitti(path_list[i])
+			data_loader = DataLoader(data, batch_size=batch_size)
+			if (len(dataset) == 1):
+				return data_loader
+			else:
+				combined_data.append(data)
+				del data_loader
+				del data
+		if(dataset[i] == 'cityscapes'):
+			data = cityscapes.CityScapes(path_list[i])
+			data_loader = DataLoader(data, batch_size=batch_size)
+			if (len(dataset) == 1):
+				return data_loader
+			else:
+				combined_data.append(data)
+				del data_loader
+				del data
+
+		if(dataset[i] == 'ourdataset'):
+			data = ourdataset.Dataset(path_list[i])
+			data_loader = DataLoader(data, batch_size=batch_size)
+			if (len(dataset) == 1):
+				return data_loader
+			else:
+				combined_data.append(data)
+				del data_loader
+				del data
+
+	data_loader = DataLoader(
+		ConcatDataset(combined_data),
+		batch_size=batch_size, shuffle=True)
+	return data_loader
